c6eaea8e78b398920d7f029a93ee30dfd8c365fc
XiaoQi <60797858+wzhou29@users.noreply.github.com>
2023-05-09 02:08:13 -0400

    Add files via upload

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -1,14 +1,14 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-INFURA_API_KEY = "https://goerli.infura.io/v3/4cb5c682ebdd425587d95c970ab2a173"
+INFURA_API_KEY = "https://sepolia.infura.io/v3/4cb5c682ebdd425587d95c970ab2a173"
 MNEMONIC = "nature stone tent harvest body loan absurd type hawk seminar absorb option"
 
 
 module.exports = {
   networks: {
-    goerli: {
+    sepolia:{
       provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
-      network_id: '5',
-      gas: 5500000,
+      network_id: '11155111',
+      gas: 4465030
     },
     development: {
       host: "127.0.0.1",
